3b7c1d8d2f344cbb57a56b3c59f70ea785bd4e2a
fares <djefaflia.fares@gmail.com>
2025-02-08 22:19:38 +0100

    download frameworks for ui: dashboard & users #11 #9

---
+++ admin/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p>Some default layout content shared across all pages</p>
+    <slot />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+</template>
