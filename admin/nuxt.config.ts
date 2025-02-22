b32be5259dcb94cb7b12bb4ef495f8b159cfd596
fares <djefaflia.fares@gmail.com>
2025-02-22 22:53:43 +0100

    Add Tailwind CSS integration and update dependencies in admin module #11 #9

--- admin/nuxt.config.ts
+++ admin/nuxt.config.ts
@@ -1,18 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@pinia/nuxt',
-    ['@nuxtjs/google-fonts', {
-        families: {
-          Inter: {
-            wght: '100..900',
-          }
-        },
-        display: 'swap',
-        preconnect: true
-    }],
-  ]
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: [['@nuxtjs/google-fonts', {
+      families: {
+        Inter: {
+          wght: '100..900',
+        }
+      },
+      display: 'swap',
+      preconnect: true
+  }], '@pinia/nuxt']
+  
 })
