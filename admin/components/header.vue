0a81a934f65101af7f2160bc4312d999288a2bfc
fares <djefaflia.fares@gmail.com>
2025-03-02 00:09:25 +0100

    Enhance admin header component: add conditional rendering for user login/logout state and implement logout functionality #10 #14

--- admin/components/header.vue
+++ admin/components/header.vue
@@ -4,9 +4,29 @@
       <NuxtLink to="/">
         <img class="w-[150px]" src="~/assets/img/Marketly.png">
       </NuxtLink>
-      <div class="bg-02 text-01 flex items-center justify-center rounded-[3px] w-[30px] h-[30px] cursor-pointer">
-        <i class="fi fi-rs-user flex h-[16px]"></i>
+      <div class="flex">
+        <div v-if="!login" class="bg-02 text-01 flex items-center justify-center rounded-[3px] w-[30px] h-[30px] cursor-pointer">
+          <i class="fi fi-rs-user flex h-[16px]"></i>
+        </div>
+        <div v-else @click="logout" class="bg-02 text-01 flex items-center justify-center rounded-[3px] w-[30px] h-[30px] cursor-pointer">
+          <i class="fi fi-br-sign-out-alt flex h-[16px]"></i>
+        </div>
       </div>
     </section>
   </header>
 </template>
+
+<script setup>
+import { computed } from 'vue';
+
+const auth = useAuthStore();
+const login = computed(() => auth.login)
+
+async function logout(){
+  const token = useCookie('token');
+  token.value = null;
+  auth.login = false;
+  await refreshNuxtData();
+  await navigateTo('/login');
+}
+</script>
